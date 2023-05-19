e44eb0a92a34e8a48666311fcb9d960f349a55a6
Felipe Pimentel <felipekdra@gmail.com>
2023-05-19 19:48:57 -0300

    feat: init mobile project

---
+++ mobile/App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text-5xl font-bold text-gray-50">Ai que diliça!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
