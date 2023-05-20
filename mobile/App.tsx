e3a9044440bfee41a61a5283e12f56421e0d0d74
Felipe Pimentel <felipekdra@gmail.com>
2023-05-19 21:39:07 -0300

    feat: create home screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
 import {
   useFonts,
@@ -8,6 +8,13 @@ import {
 } from '@expo-google-fonts/roboto'
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import bgBlur from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -21,9 +28,36 @@ export default function App() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="text-5xl font-bold text-gray-50">Ai que diliça!</Text>
+    <ImageBackground
+      source={bgBlur}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity className="rounded-full bg-green-500 px-5 py-3">
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   )
 }
